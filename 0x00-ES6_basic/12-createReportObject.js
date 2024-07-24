67479c3bd69aefa03b640c12e21198126d876c59
shaimaa <shaimafikry@yahoo.com>
2024-07-25 00:30:37 +0300

    fix 12 task

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,3 +1,7 @@
 export default function createReportObject(employeesList) {
-
+  const data = {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments() { return Object.keys(this.allEmployees).length; },
+  };
+  return data;
 }
