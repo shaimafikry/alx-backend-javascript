0648c893ae19f922e5d515ac17e7980625f0640a
shaimaa <shaimafikry@yahoo.com>
2024-07-26 16:50:33 +0300

    fix 100 task

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,12 @@
 export default function createIteratorObject(report) {
-
+  const nameList = [];
+  // get the first parameter of the object report
+  const keyFirst = Object.keys(report)[0];
+  // loop through the values of the key
+  for (const value of Object.values(report[keyFirst])) {
+    // spread the list before pushing i to the new one
+    // as the values are lists
+    nameList.push(...value);
+  }
+  return nameList;
 }
