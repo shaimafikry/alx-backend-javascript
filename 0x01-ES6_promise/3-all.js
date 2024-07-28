a62a8a1a11c90011495e6ab94e4d5291c16caf0d
shaimaa <shaimafikry@yahoo.com>
2024-07-29 00:27:04 +0300

    add all the dir

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -15,5 +15,5 @@ export default function handleProfileSignup() {
     .then(([value1, value2]) => {
       console.log(`${value1.body}`, `${value2.firstName}`, `${value2.lastName}`);
     })
-    .catch(() => { console.log('Signup system offline to the console'); });
+    .catch(() => { console.log('Signup system offline'); });
 }
