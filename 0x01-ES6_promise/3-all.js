dcc7ac598f15da76e1048aa952a45f7af8aab050
shaimaa <shaimafikry@yahoo.com>
2024-07-29 00:19:56 +0300

    add all the dir

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -13,7 +13,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([value1, value2]) => {
-      console.log(value1.body, value2.firstName, value2.lastName);
+      console.log(`${value1.body}`, `${value2.firstName}`, `${value2.lastName}`);
     })
     .catch(() => { console.log('Signup system offline to the console'); });
 }
