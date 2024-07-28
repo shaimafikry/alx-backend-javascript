b2bda3f9d1ac08b215e4e3c1cf713d48990d889e
shaimaa <shaimafikry@yahoo.com>
2024-07-28 12:43:34 +0300

    add all the dir

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,15 @@
+// Using the function prototype below
+
+// function handleResponseFromAPI(promise)
+// Append three handlers to the function:
+
+// When the Promise resolves, return an object with the following attributes
+// status: 200
+// body: success
+// When the Promise rejects, return an empty Error object
+// For every resolution, log Got a response from the API to the console
+export default function handleResponseFromAPI(promise) {
+  return promise.then(() => {
+    console.log('Got a response from the API');
+  });
+}
