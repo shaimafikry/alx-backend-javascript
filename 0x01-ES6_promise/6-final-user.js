2a8765024170cfa214e45977ab2bf9a309ff5869
shaimaa <shaimafikry@yahoo.com>
2024-07-29 11:52:43 +0300

    file 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -14,5 +14,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(`${firstName}`, `${lastName}`), uploadPhoto(`${fileName}`)]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      console.log(results);
+      return results;
+    });
 }
