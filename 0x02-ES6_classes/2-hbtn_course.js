cdb8d683cd5a99e83919f8b50619662b8aaf79de
shaimaa <shaimafikry@yahoo.com>
2024-08-08 04:39:25 +0300

    add 3#

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,61 @@
+// Implement a class named HolbertonCourse:
+
+// Constructor attributes:
+// name (String)
+// length (Number)
+// students (array of Strings)
+// Make sure to verify the type of attributes during object creation
+// Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
+// Implement a getter and setter for each attribute.
+
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+
+    if (!Array.isArray(students)) {
+      throw new TypeError('students must be a array of strings');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students)) {
+      throw new TypeError('students must be a array of strings');
+    }
+    this._students = students;
+  }
+}
