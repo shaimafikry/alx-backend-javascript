28cbc8169c405e67f2b193033fe66ffc825aefdf
shaimaa <shaimafikry@yahoo.com>
2024-08-09 07:50:13 +0300

    add 8#

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,21 @@
+// Implement a class named HolbertonClass:
+
+// Constructor attributes:
+// size (Number)
+// location (String)
+// Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
+// When the class is cast into a Number, it should return the size.
+// When the class is cast into a String, it should return the location.
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    return this._location;
+  }
+}
