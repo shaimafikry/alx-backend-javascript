bd63579a53f0b368cfc23601d0b6719e61e4cd63
shaimaa <shaimafikry@yahoo.com>
2024-08-09 11:16:47 +0300

    add 11#

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,21 @@
+// Implement a class named Car:
+
+// Constructor attributes:
+// brand (String)
+// motor (String)
+// color (String)
+// Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
+// Add a method named cloneCar. This method should return a new object of the class.
+// Hint: Symbols in ES6
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brabd = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor(this.brand, this.motor, this.color);
+  }
+}
