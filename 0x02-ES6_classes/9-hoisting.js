2e21a2317530fe428e1f45041287e19e0fd3f84d
shaimaa <shaimafikry@yahoo.com>
2024-08-09 10:54:32 +0300

    add 8#

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -14,10 +14,10 @@ export class HolbertonClass {
 }
 
 export class StudentHolberton {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = HolbertonClass;
+    this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
