d8a17fb6c5a52c4b84360ed2cd08cb6afe2f70e2
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2024-08-31 22:16:17 +0300

    Update 8-clean_set.js

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default cleanSet(set, startString) {
+    if (startString === '') {
+      return '';
+    }
+    res = [];
+    for (const i of set) {
+     if (i.startsWith(startString)) {
+         res.push(i.slice(startString.length));
+     }
+    }
+    return res.join('-');
+}
