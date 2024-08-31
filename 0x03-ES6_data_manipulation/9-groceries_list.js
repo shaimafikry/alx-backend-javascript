7510cf664229fcff05c4311203eecbb5d8356683
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2024-09-01 02:41:36 +0300

    Update 9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  moe = new Map();
+  moe.set('Apples', 10);
+  moe.set('Tomatoes', 10);
+  moe.set('Pasta', 1);
+  moe.set('Rice', 1);
+  moe.set('Banana', 5);
+  return moe;
+}
