a5585e369ca65e83d9379484e81bf5a7a85accc4
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2024-09-01 05:05:47 +0300

    Update 10-update_uniq_items.js

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,12 @@
+export default function updateUniqueItems(argMap) {
+  if (typeof argMap !== 'map') {
+    throw new Error ('argument is not a map');
+  }
+  // For each entry of the map where the quantity
+  // is 1, update the quantity to 100
+  for ( const key in argMap) {
+    if (argMap.key === 1) {
+      argMap.key = 100;
+    }
+  }
+}
