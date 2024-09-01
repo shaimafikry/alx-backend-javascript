ecef291e644d69d971c96e7dca4f0a85577ba2a3
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2024-09-01 04:51:39 +0300

    Update 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,12 +1,12 @@
-export const weakmap = WeakMap();
+export const weakMap = WeakMap();
 export function queryAPI(endpoint) {
   // define counter
   const nCount = 0;
   // ge the stored value if not set it to 0
-  const count = weakmap.get(endpoint) || 0;
+  const count = weakMap.get(endpoint) || 0;
   // add a count
   nCount = count + 1;
-  weakmap.set(endpoint, nCount);
+  weakMap.set(endpoint, nCount);
   if (nCount >= 5) {
     throw new error ('Endpoint load is high');
   }
