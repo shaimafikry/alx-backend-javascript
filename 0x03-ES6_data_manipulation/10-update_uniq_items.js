369e5439b3d6ddef4436482076c940730085e315
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2024-09-01 05:17:12 +0300

    Update 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,13 @@
 export default function updateUniqueItems(argMap) {
-  if (typeof argMap !== 'map') {
-    throw new Error ('argument is not a map');
+  if (!(argMap instanceof Map)) {
+    throw new Error('argument is not a map');
   }
   // For each entry of the map where the quantity
   // is 1, update the quantity to 100
-  for (const key in argMap) {
-    if (argMap.key === 1) {
-      argMap.key = 100;
+  // foreach in maos get the balue first
+  argMap.forEach((value, key) => {
+    if (value === 1) {
+      argMap.set(key, 100);
     }
-  }
+  });
 }
