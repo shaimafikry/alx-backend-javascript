fd0dc2cb513c5a0c0c9a5f118af58d98f0e728b2
shaimaa <shaimafikry@yahoo.com>
2024-09-19 18:52:01 +0300

    add new task

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,23 +1,23 @@
-// process.stdout.write('Welcome to Holberton School, what is your name?\n');
-// taking input
-// process.stdin.on('readable', () => {
-//   const inp = process.stdin.read();
-//   if (inp) {
-//     console.log(`Your name is: ${inp}`);
-//   }
-// });
-
-// process.stdin.on('end', () => {
-//   process.stdout.write('This important software is now closing\n');
-// });
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 // taking input
-process.stdin.on('data', (data) => {
-  const inp = data.toString();
+process.stdin.on('readable', () => {
+  const inp = process.stdin.read();
   if (inp) {
     process.stdout.write(`Your name is: ${inp}`);
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
   }
 });
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
+
+// process.stdout.write('Welcome to Holberton School, what is your name?\n');
+// // taking input
+// process.stdin.on('data', (data) => {
+//   const inp = data.toString();
+//   if (inp) {
+//     process.stdout.write(`Your name is: ${inp}`);
+//     process.stdout.write('This important software is now closing\n');
+//     process.exit();
+//   }
+// });
