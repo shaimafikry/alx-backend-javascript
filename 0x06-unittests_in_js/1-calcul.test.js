742e47694e498cd12c6b1c215eb4db04d484f15c
shaimaa <shaimafikry@yahoo.com>
2024-09-28 18:56:35 +0300

    add SECOND task

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -11,11 +11,16 @@ describe('math function that return RESULT of two number based on type', functio
 			assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
 		});
 	});
+});
+describe('math function that return RESULT of two number based on type', function() {
 	describe('#SUBTRACT', function (){
 		it('should return 3 when given 7, 3.7', function() {
 			assert.strictEqual(calculateNumber('SUBTRACT', 7, 3.7), 3);
 		});
 	});
+});
+
+describe('math function that return RESULT of two number based on type', function() {
 	describe('#DIVIDE ', function (){
 		it('should return 6 when given 12.2, 2', function() {
 			assert.strictEqual(calculateNumber('DIVIDE', 12.2, 2), 6);
