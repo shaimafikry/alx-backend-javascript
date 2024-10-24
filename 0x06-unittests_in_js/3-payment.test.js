6a88b2b5137374f00ca6970383109cae79acee9e
shaimaa <shaimafikry@yahoo.com>
2024-10-24 14:19:37 +0300

    try fail test

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,14 +1,29 @@
-const sendPaymentRequestToApi = require('./3-payment');
-const Utils = require('./utils');
-const sinon = require('sinon');
+import sendPaymentRequestToApi from './3-payment.js';
+import Utils from './utils.js';
+import { expect } from 'chai';
+import sinon from 'sinon';
 
-describe('sendPaymentRequestToApi', async () => {
-  const { expect } = await import('chai');
-  
-  it('should use Utils.calculateNumber', () => {
-    const spy = sinon.spy(Utils, 'calculateNumber');
-    sendPaymentRequestToApi(100, 20);
-    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+describe('sendPaymentRequestToApi', () => {
+  let spy;
+  // specifiy what to spy on
+  beforeEach(() => {
+    spy = sinon.spy(Utils, 'calculateNumber');
+  });
+  // clear spy ater test
+  afterEach(() => {
     spy.restore();
+  });
+  // call function
+  it('should call Utils.calculateNumber with correct arguments', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.calledOnce).to.be.true;
+
+    // check calling
+    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+  });
+
+  it('should fail if Utils.calculateNumber is not called', () => {
+    //no calling
+    expect(spy.notCalled).to.be.true;
   });
 });
