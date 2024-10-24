b414fe477ce85a46970697b80386fbc10da03f50
shaimaa <shaimafikry@yahoo.com>
2024-10-24 14:46:28 +0300

    fail success

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,21 +1,31 @@
 const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 const sinon = require('sinon');
+const { expect } = require('chai');
 
-describe('sendPaymentRequestToApi', async () => {
-  const { expect } = await import('chai');
-  
+describe('sendPaymentRequestToApi', () => {
+
   it('should use Utils.calculateNumber', () => {
     const spy = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
     expect(spy.calledWith('SUM', 100, 20)).to.be.true;
     spy.restore();
   });
+
   it('should fail if Utils.calculateNumber is not called', () => {
-    //another try without calling
     const spy = sinon.spy(Utils, 'calculateNumber');
-    // not call
-		expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+    
+    // Call the function but ensure it does not use Utils.calculateNumber
+    // To simulate this, you can temporarily overwrite the function
+    Utils.calculateNumber = () => {}; // Overwrite with a no-op function
+    
+    // Call the function
+    sendPaymentRequestToApi(100, 20);
+
+    // Check that the spy was not called
+    expect(spy.notCalled).to.be.true;
+
+    // Restore the original function
     spy.restore();
   });
 });
